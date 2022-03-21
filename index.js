5bcb11469a548fa72b8bfc072c68107840b76399
BLooDBRothER <computertechnology446@gmail.com>
2022-03-21 17:42:04 +0530

    Create route for index page

--- index.js
+++ index.js
@@ -5,7 +5,27 @@ const path = require('path');
 const PORT = process.env.port || 5000;
 
 const server = http.createServer((req, res) => {
+    const contentType = {};
+    contentType['Content-Type'] = 'text/html';
+    let filePath = path.join(__dirname, '/src', req.url === '/' ? 'index.html' : req.url);
     
+    const extName = path.extname(filePath);
+
+    switch(extName){
+        case '.html':
+            contentType['Content-Type'] = 'text/html';
+        break;
+        case '.css':
+            contentType['Content-Type'] = 'text/css';
+            break;
+        case '.js':
+            contentType['Content-Type'] = 'text/javascript';
+    }
+
+    fs.readFile(filePath, (err, data) => {
+        res.writeHead(200, contentType);
+        res.end(data);
+    });
 });
 
 server.listen(PORT, (err) => {
