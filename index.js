7c01345e06a31411d61d1c1bacb5ce60abafb753
BLooDBRothER <computertechnology446@gmail.com>
2022-08-03 10:23:33 +0530

    Rewrite with express logic

--- index.js
+++ index.js
@@ -1,42 +1,6 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const express = require('express');
+const app = express();
 
-const PORT = process.env.port || 5000;
+app.use('/', express.static('src'));
 
-const server = http.createServer((req, res) => {
-    const contentType = {};
-    contentType['Content-Type'] = 'text/html';
-
-    let filePath = path.join(__dirname, '/src', req.url === '/' ? 'index.html' : req.url);
-    
-    
-    const extName = path.extname(filePath);
-
-    if(extName === '.html' && !(filePath.includes('index.html') || filePath.includes('about.html') || filePath.includes('contact-me.html'))){
-        filePath = path.join(__dirname, '/src', '404.html');
-    }
-
-    switch(extName){
-        case '.html':
-            contentType['Content-Type'] = 'text/html';
-        break;
-        case '.css':
-            contentType['Content-Type'] = 'text/css';
-            break;
-        case '.js':
-            contentType['Content-Type'] = 'text/javascript';
-            break;
-        case '.jpg':
-            contentType['Content-Type'] = 'image/jpg'
-    }
-
-    fs.readFile(filePath, (err, data) => {
-        res.writeHead(200, contentType);
-        res.end(data);
-    });
-});
-
-server.listen(PORT, (err) => {
-    console.log("Active on http://localhost:5000");
-});
+app.listen(3000, (err) => {console.log('active @ localhost:3000')});
